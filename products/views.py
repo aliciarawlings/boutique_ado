54a63547ed6b8f27fb1200c3accef6a6ed6935d0
aliciarawlings <lilyrawlings123@gmail.com>
2020-08-10 19:25:58 +0000

    added products view to twmplates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    """A view show all products page, including sortingand search queries  """
+    prodcuts = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products.html', context)
