0fcdcd5d59e295d670002d085e1dd4afb75efad0
aliciarawlings <lilyrawlings123@gmail.com>
2020-08-12 16:39:08 +0000

    sorting products

--- products/views.py
+++ products/views.py
@@ -21,17 +21,18 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
-                    sortkey = f' -{sortkey}'
-                products = products.order_by(sortkey)
-
-            if 'category' in request.GET:
-                categories = request.GET['category'].split(',')
-                products = products.filter(category__name__in=categories)
-                categories = Category.objects.filter(name__in=categories)
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+            
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -42,11 +43,7 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
- 
     current_sorting = f'{sort}_{direction}'
-         
-
-    
 
     context = {
         'products': products,
