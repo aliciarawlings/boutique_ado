13d69ea93107fd984b537ce3548dd7cc735e3803
aliciarawlings <lilyrawlings123@gmail.com>
2020-08-13 22:38:31 +0000

    input quantity buttons added

--- bag/views.py
+++ bag/views.py
@@ -6,14 +6,18 @@ def view_bag(request):
     return render(request, 'bag/bag.html')
 
 
-
-
 def add_to_bag(request,item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['size']
     bag = request.session.get('bag', {})
 
+
+    
+    
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
